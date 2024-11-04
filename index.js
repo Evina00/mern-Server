04c12cb89ce9724832b9040ae5a342c6b7bac13e
Evina00 <penny121340@gmail.com>
2024-11-04 22:49:16 +0800

    Initialt commit

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ require("./config/passport")(passport);
 const cors = require("cors");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mernDB")
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connecting to mongodb...");
   })
