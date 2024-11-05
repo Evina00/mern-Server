8d44c5118f0a69a9f87f3d28882408bb51a7a8ba
Evina00 <penny121340@gmail.com>
2024-11-05 23:00:13 +0800

    update index.js

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ require("./config/passport")(passport);
 const cors = require("cors");
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connecting to mongodb...");
   })
